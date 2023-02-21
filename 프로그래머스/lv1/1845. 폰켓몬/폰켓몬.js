63b79dc3d22f5cc8d826a561cb57f7ec6c472abb
Sowon Choi <48286887+ssowon@users.noreply.github.com>
2023-02-22 00:57:41 +0900

    [level 1] Title: 폰켓몬, Time: 0.23 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
@@ -1,7 +1,7 @@
 function solution(nums) {
     let answer = 0;
-    let result1 = [...new Set(nums)];
-    if(result1.length < nums.length/2) answer = result1.length
+    let noDuplicatesNums = [...new Set(nums)];
+    if(noDuplicatesNums.length < nums.length/2) answer = noDuplicatesNums.length
     else answer = nums.length/2
     
     return answer
