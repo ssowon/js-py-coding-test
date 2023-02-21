b8765291f0503c41ef9cb183df16a1399b12353e
Sowon Choi <48286887+ssowon@users.noreply.github.com>
2023-02-22 00:54:33 +0900

    [level 1] Title: 폰켓몬, Time: 0.33 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
@@ -0,0 +1,8 @@
+function solution(nums) {
+    let answer = 0;
+    let result1 = [...new Set(nums)];
+    if(result1.length < nums.length/2) answer = result1.length
+    else answer = parseInt(nums.length/2)
+    
+    return answer
+}
