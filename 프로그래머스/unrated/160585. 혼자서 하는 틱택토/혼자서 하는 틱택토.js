b4b65289bd0217c70ee903cdd5abcf2c81e29612
Sowon Choi <48286887+ssowon@users.noreply.github.com>
2023-02-23 21:56:10 +0900

    [unrated] Title: 혼자서 하는 틱택토, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.js
+++ 프로그래머스/unrated/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.js
@@ -1,28 +1,31 @@
 function solution(board) {
-    var answer = -1;
-    let positionO = [], positionX = []
+    let turnCountO = 0, turnCountX = 0
     for (let i in board) {
         for(let j in board[i]) {
-            if(board[i][j] == 'O') positionO.push([parseInt(i)+1, parseInt(j)+1])
-            if(board[i][j] == 'X') positionX.push([parseInt(i)+1, parseInt(j)+1])
+            if(board[i][j] == 'O') turnCountO++
+            else if(board[i][j] == 'X') turnCountX++
         }
     }
     
-    if(positionO.length < positionX.length) return 0
-    else if(positionO.length - positionX.length > 1) return 0
-    else if(positionO.length == positionX.length && isFinish(board) == 'O') return 0
-    else if(positionO.length > positionX.length && isFinish(board) == 'X') return 0
+    if(turnCountO < turnCountX) return 0
+    else if(turnCountO - turnCountX > 1) return 0
+    else if(turnCountO == turnCountX && getWinner(board) == 'O') return 0
+    else if(turnCountO > turnCountX && getWinner(board) == 'X') return 0
     return 1;
 }
 
-function isFinish(Board) {
+function getWinner(board) {
     // 직선
     for(i in [0,1,2]) {
-        if(Board[i][0] != '.' && Board[i][0] == Board[i][1] && Board[i][1] == Board[i][2]) return Board[i][0]
-        if(Board[0][i] != '.' && Board[0][i] == Board[1][i] && Board[1][i] == Board[2][i]) return Board[0][i]
+        if(board[i][0] != '.' && isSame(board[i][0], board[i][1], board[i][2])) return board[i][0]
+        if(board[0][i] != '.' && isSame(board[0][i], board[1][i], board[2][i])) return board[0][i]
     }
     //대각선
-    if(Board[0][0] != '.' && Board[0][0] == Board[1][1] && Board[1][1] == Board[2][2]) return Board[0][0]
-    if(Board[0][2] != '.' && Board[0][2] == Board[1][1] && Board[1][1] == Board[2][0]) return Board[0][2]
+    if(board[0][0] != '.' && isSame(board[0][0], board[1][1], board[2][2])) return board[0][0]
+    if(board[0][2] != '.' && isSame(board[0][2], board[1][1], board[2][0])) return board[0][2]
     return false;
+}
+
+function isSame(element1, element2, element3) {
+    return element1 == element2 && element2 == element3;
 }
