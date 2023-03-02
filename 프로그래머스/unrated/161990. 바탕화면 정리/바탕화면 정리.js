f2292a41e555784068b60fd6901a1ba641acf794
Sowon Choi <48286887+ssowon@users.noreply.github.com>
2023-03-03 00:04:19 +0900

    [unrated] Title: 바탕화면 정리, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.js
+++ 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.js
@@ -1,18 +1,17 @@
 function solution(wallpaper) {
-    var answer = [];
-    let lux = -1, luy = -1, rdx = 0, rdy = 0;
+    let [lux, luy, rdx, rdy] = [-1, -1, 0, 0];
     for (let i in wallpaper) {
         if(wallpaper[i].includes('#')) {
-            miny = wallpaper[i].indexOf('#')
-            maxy = wallpaper[i].lastIndexOf('#')
-            y = Number(i)
-            if(lux == -1) lux = y
-            rdx = y+1
+            let [miny, maxy] = [wallpaper[i].indexOf('#'), wallpaper[i].lastIndexOf('#')]
+            let x = Number(i)
+            
+            if(lux == -1) lux = x
+            rdx = x + 1
             
             if(luy == -1) luy = miny
             else if(luy > miny) luy = miny;
             
-            if(rdy <= maxy) rdy = maxy+1;
+            if(rdy <= maxy) rdy = maxy + 1;
         }
     }
     return [lux,luy,rdx,rdy];
