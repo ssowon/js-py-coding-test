7d0cb0a15ef530aa3d718ef70457a4c3b74a23dd
Sowon Choi <48286887+ssowon@users.noreply.github.com>
2023-03-04 23:28:02 +0900

    [unrated] Title: 대충 만든 자판, Time: 1.54 ms, Memory: 34 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.js
@@ -0,0 +1,24 @@
+function solution(keymap, targets) {
+    var answer = [];
+    let alphabetCounts = {};
+    for(key of keymap) {
+        for(seq in key) {
+            let alphabet = key[seq++]
+            if(alphabetCounts[alphabet] && alphabetCounts[alphabet] > seq) alphabetCounts[alphabet] = seq;
+            else if(!alphabetCounts[alphabet]) alphabetCounts[alphabet] = seq;
+        }
+    }
+    
+    for(target of targets) {
+        let count = -1;
+        for(element of target) {
+            if(alphabetCounts[element]) {
+                count += alphabetCounts[element]  
+            } else {
+                count = -1; break;
+            }
+        }
+        answer.push(count == -1 ? -1 : count+1)
+    }
+    return answer
+}
