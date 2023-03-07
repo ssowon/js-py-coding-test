65898b441c1ee49ef8747d9e1c18a49c14c585a7
Sowon Choi <48286887+ssowon@users.noreply.github.com>
2023-03-08 00:44:09 +0900

    [level 0] Title: 옹알이 (1), Time: 0.16 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.js
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -1,19 +1,15 @@
 function solution(babbling) {
     var answer = 0;
     for(let i of babbling) {
-        console.log(i)
-        while(i.length > 0) {
-            if(i.length < 2) {
-                break;
-            } else if(i.length > 1) {
-                if(i.substr(0,3) == "aya") i = i.slice(3)
-                else if(i.substr(0,2) == "ye") i = i.slice(2)
-                else if(i.substr(0,3) == "woo") i = i.slice(3)
-                else if(i.substr(0,2) == "ma") i = i.slice(2)
-                else break;
-            }
+        while(i.length) {
+            if(i.substr(0,3) == "aya" || i.substr(0,3) == "woo") i = i.slice(3)
+            else if(i.substr(0,2) == "ye" || i.substr(0,2) == "ma") i = i.slice(2)
+            else break;
+
             if(i.length == 0) {
                 answer++;
+                break;
+            } else if (i.length == 1) {
                 break;
             }
         }
