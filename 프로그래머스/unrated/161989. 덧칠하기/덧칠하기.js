8bbd19b523c000363ea648ae7808dc92484a3651
Sowon Choi <48286887+ssowon@users.noreply.github.com>
2023-03-11 23:50:37 +0900

    [unrated] Title: 덧칠하기, Time: 3.57 ms, Memory: 39.8 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.js
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.js
@@ -1,11 +1,6 @@
 function solution(n, m, section) {
     var answer = 0;
     var colored_length = 0;
-    // while (section.length > 0) {
-    //     colored_length = section[0] + m - 1;
-    //     section = section.filter(element => element > colored_length);
-    //     answer++;
-    // }
     
     for(i of section) {
         if(i > colored_length) {
