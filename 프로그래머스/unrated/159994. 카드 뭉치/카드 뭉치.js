07b00de8a901e145ef106feeee7cb19d26a02d2c
Sowon Choi <48286887+ssowon@users.noreply.github.com>
2023-03-12 00:12:39 +0900

    [unrated] Title: 카드 뭉치, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
@@ -0,0 +1,10 @@
+function solution(cards1, cards2, goal) {
+    var answer = '';
+    
+    for (let word of goal) {
+        if (cards1[0] == word) cards1.shift();
+        else if (cards2[0] == word) cards2.shift();
+        else return "No";
+    }
+    return "Yes";
+}
